e8671b9c39c7402de1be370737fcc3c27ee797ce
Jeremy Field <jeremy.field@gmail.com>
2022-02-18 23:27:06 +1100

    sort by meanVal compiles, not yet tested

--- src/bakery/mixer.nim
+++ src/bakery/mixer.nim
@@ -1,4 +1,4 @@
-import std / [options, tables]
+import std / [options, tables, sugar]
 
 import ingredients
 
@@ -9,15 +9,31 @@ type
     
   Grouped[K, V] = OrderedTable[Option[K], seq[Option[V]]]
 
-proc points*[X, Y](sh: Shopping, x, y: string): seq[OptionalPair[X, Y]] =
+proc points*[X, Y](sh: Shopping; x, y: string): seq[OptionalPair[X, Y]] =
   ## TODO generalise to >2 vals?
   for r in sh.data.getElems:
     result.add((get[X](sh, r, x), get[Y](sh, r, y)))
 
-proc grouped*[K, V](data: seq[OptionalPair[K, V]]): Grouped[K, V] =
-  ## Keys in order of first insertion for the moment.
+proc group*[K, V](data: seq[OptionalPair[K, V]]): Grouped[K, V] =
+  ## Keys in order of first insertion for the moment. Can use OrderedTable.sort.
   for (k, v) in data:
     if result.hasKey(k):
       result[k].add(v)
     else:
       result[k] = @[v]
+
+proc mean*[V: int|float](vs: seq[Option[V]]): float =
+  var
+    sum: float
+    n: int
+  for v in vs:
+    if v.isSome:
+      sum += v.get
+      inc n
+  if n > 0:  
+    return sum/n.float
+  return n.float
+
+proc meanVal*[K, V](a, b: (Option[K], seq[Option[V]])): int =
+  ## For use with Grouped.sort (which comes from OrderedTable.sort)
+  cmp(a[1].mean, b[1].mean)
--- src/examples/oven.nim
+++ src/examples/oven.nim
@@ -1,13 +1,24 @@
 ## Render static visualisation, to be picked up by DOM manipulation when js available.
 
-import std / [json, options, tables]
+import std / [json, options, tables, strformat]
 import karax / [karaxdsl, vdom]
 import .. / bakery / [ingredients, mixer]
 
 proc bake*(sh: Shopping): string =
-  let ps = points[int, float](sh, "ANAESTHETIST", "TEMPERATURE_INITIAL")
-  let vnode = buildHtml(tdiv()):
-    ul:
-      for (a,t) in ps.grouped.pairs:
-        li: text $a & $t
+  let
+    d = (w: 1000, h: 500)
+    m = (t: 30, r: 150, b: 0, l: 20)
+    s = (w: d.w + m.l + m.r, h: d.h + m.t + m.b)
+    
+    ps = points[int, float](sh, "ANAESTHETIST", "TEMPERATURE_INITIAL")
+    
+  var psg = ps.group # sort in place!
+  psg.sort(meanVal)
+  
+  let vnode = buildHtml(svg(width="100%", viewBox=fmt"{-m.l} {-m.t} {s.w} {s.h}")):
+    g:
+      # OrderedTable.sort only sorts keys; will need to impl own to sort by vals.
+      for (k, vs) in psg.pairs:
+        for (i, v) in vs.pairs:
+          circle(cx = $i, cy = $i, r = $4)
   result = $vnode
